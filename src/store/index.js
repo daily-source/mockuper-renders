134c438137882d10a6951fcfee9e12720e921900
Camilo Delvasto <camilodelvasto@gmail.com>
2018-12-18 22:44:09 -0500

    Improve store for this mockup

--- src/store/index.js
+++ src/store/index.js
@@ -11,9 +11,9 @@ export default new Vuex.Store({
     fullPath: '',
     common: {},
     nonprofit: {
-      "43138428": {
-        EIN: 43138428,
-        NAME: 'MELROSE MASSACHUSETTS TELEVISION',
+      "237069110": {
+        EIN: 237069110,
+        NAME: 'OXFAM AMERICA',
         STREET: '360 MAIN ST',
         CITY: 'MELROSE',
         STATE: 'MA',
@@ -26,7 +26,7 @@ export default new Vuex.Store({
         SORT_NAME: '0',
         validated: true,
         data: {
-          name: 'MELROSE MASSACHUSETTS TELEVISION',
+          name: 'OXFAM AMERICA',
           hero: {
             src: `${baseUrl}img/5672087be97cb2bb8e59ad897a14ea35.jpg`,
           },
@@ -128,7 +128,7 @@ export default new Vuex.Store({
     fundraiser: {
       fundraiser_id: 254,
       user_id: 721,
-      nonprofit_ein: 43138428,
+      nonprofit_ein: 237069110,
       name: 'Tutoring children for the future',
       description: 'Praesent bibendum gravida diam faucibus lobortis. Nulla a leo id mauris fringilla dictum dictum vitae ligula. Etiam maximus turpis vel dolor venenatis malesuada. Duis eget rutrum elit. Fusce elementum tempor condimentum. Cras feugiat consectetur sem, non scelerisque nisi. Donec eu magna a ante imperdiet accumsan. In tempus laoreet mi aliquet fermentum. Nullam nec cursus est. Nulla dapibus dolor fermentum hendrerit lobortis. ',
       intro_text: '<p>Mauris nec pharetra massa. Nulla facilisi. Nulla at scelerisque ante. Nam at nisi vel odio fringilla ultrices. Phasellus quis nunc consectetur, ullamcorper sapien ut, condimentum risus. Quisque feugiat gravida faucibus. Praesent eget nisl bibendum, suscipit odio vitae, euismod nisl. Ut varius aliquam semper. Quisque ac aliquam leo. Suspendisse et rhoncus neque.</p><p>Ut eleifend leo eu est lobortis, nec lobortis magna fringilla. Nullam nec condimentum libero. Vivamus et tristique est, vitae venenatis tellus. Duis tempus nibh justo, at mattis turpis rhoncus ut. Morbi ac est id ex feugiat lacinia a sit amet arcu. Maecenas sit amet eros tellus. Ut sit amet mattis neque. Nunc leo risus, vulputate pellentesque interdum facilisis, suscipit ac sapien. Morbi volutpat ac turpis a blandit. Maecenas ultricies quis nunc vitae aliquam.</p><p>Nulla facilisi. Proin congue imperdiet volutpat. Vivamus quis enim a neque consequat dictum et sed lacus. Vestibulum aliquam vel metus non egestas. Duis ligula lacus, tincidunt a quam non, porta cursus elit. Nunc at fermentum quam. Duis ut elit justo. In eget pulvinar sapien. Cras interdum orci a pellentesque faucibus. Maecenas nec enim ac velit commodo faucibus. Nam vitae lorem sollicitudin, sodales eros ac, congue nibh. Aenean laoreet semper convallis. Vestibulum pharetra, purus non elementum convallis, lectus neque consequat magna, et dictum metus ante non lacus. Quisque scelerisque ex sit amet euismod semper.</p><p>Integer sed ante vitae arcu luctus ultrices. Donec iaculis est ut turpis malesuada, eu bibendum libero malesuada. Sed et fermentum est. Sed sit amet consequat nibh. Mauris posuere mi eu sapien vestibulum, ac viverra urna dictum. Vestibulum finibus enim et pharetra fringilla. Curabitur molestie, dui vitae lobortis dignissim, elit massa facilisis arcu, a lobortis odio felis et sem. Mauris lectus quam, varius in scelerisque ac, condimentum eget nisi. Sed ac vulputate purus, sit amet aliquam lacus. Sed eget euismod arcu. Quisque maximus, sapien at blandit vestibulum, lectus purus sagittis dui, vitae lacinia arcu sapien ut urna. Nunc laoreet mi eget libero condimentum ultricies. Phasellus quam augue, congue ac est ac, rutrum tincidunt nibh.</p><p>Nam quis ligula nibh. Duis viverra at diam a sollicitudin. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque ullamcorper nisl et metus dictum viverra. Nam vel iaculis est. Nunc elementum in mauris convallis auctor. Mauris ac enim felis. Sed eget venenatis purus. Nam varius mattis massa.</p><p>Integer rhoncus pretium molestie. Morbi bibendum iaculis volutpat. Morbi id velit lobortis, porttitor neque et, ornare turpis. Sed at tincidunt enim, nec lobortis sapien. Donec posuere, ante at rhoncus euismod, enim libero porta turpis, vel blandit lacus lacus tempus urna. Ut quis lacus eleifend, interdum lorem at, sollicitudin ligula. Suspendisse vel lectus consectetur, gravida dui blandit, molestie erat. Phasellus sit amet mauris sapien. Curabitur vitae tempor elit. Duis id ultricies leo. Nunc quam erat, euismod viverra facilisis eu, rutrum viverra arcu. Quisque porta, orci eget congue hendrerit, quam libero porta diam, vel dictum mauris risus eget nibh. Ut at mi risus. Aliquam in dignissim augue. Vestibulum eros leo, sollicitudin consectetur ipsum ac, rutrum lacinia tortor.</p><p>Nam ullamcorper arcu sed euismod cursus. Nam pretium ultricies lacus. Aliquam auctor diam velit, id consectetur nulla ultrices a. Maecenas pretium arcu in placerat rutrum. Morbi fringilla lectus id magna ullamcorper, eget gravida odio hendrerit. Vivamus non aliquet eros. Phasellus eget lectus at libero tincidunt sollicitudin. Nam interdum vitae justo molestie imperdiet.</p>',
@@ -177,10 +177,10 @@ export default new Vuex.Store({
         updatesCount: 10
       },
       Nonprofit: {
-        logo_square: `${baseUrl}img/fundraiser-feed-the-children-logo-square.png`,
-        NAME: 'MELROSE MASSACHUSETTS TELEVISION',
-        website: 'https://melrose-massachusetts.tv',
-        EIN: 43138428,
+        logo_square: `${baseUrl}img/oxfam-square.png`,
+        NAME: 'OXFAM AMERICA',
+        website: 'https://oxfam.org',
+        EIN: 237069110,
         STREET: '360 MAIN ST',
         CITY: 'MELROSE',
         STATE: 'MA',
@@ -192,9 +192,9 @@ export default new Vuex.Store({
         NTEE_CD: '0',
         SORT_NAME: '0',
         data: {
-          name: "MELROSE MASSACHUSETTS TELEVISION TV",
+          name: "OXFAM AMERICA",
           logo: {
-            src: `${baseUrl}img/fundraiser-feed-the-children-logo.png`,
+            src: `${baseUrl}img/oxfam-square.png`,
           },
           media: []
         }
@@ -523,32 +523,32 @@ export default new Vuex.Store({
     donations: {
       byAmount: {
         data: [
-          { fundraiser_id: 254, nonprofit_ein: 43138428, user_id: 721, amountInCents: 4500, User: { firstName: "Francisca", lastName: "Morana", nickname: "Fran", avatar: { src: `${baseUrl}img/donor-1.jpg` }}, fullname: 'Francisca Morana', firstName: 'Francisca', lastName: 'Morana', avatar: `${baseUrl}img/donor-1.jpg`, createdAt: 1521504544000, public: true, },
-          { fundraiser_id: 254, nonprofit_ein: 43138428, user_id: 722, amountInCents: 3000, fullname: 'Ariadna Beckenbauer', firstName: 'Ariadna', lastName: 'Beckenbauer', createdAt: 1526504544000, public: true, },
-          { fundraiser_id: 254, nonprofit_ein: 43138428, user_id: 723, amountInCents: 3500, fullname: 'Andreas Higgins', firstName: 'Andreas', lastName: 'Higgins', User: { avatar: { src: `${baseUrl}img/donor-4.jpg` } }, createdAt: 1539504944000, public: true, },
-          { fundraiser_id: 254, nonprofit_ein: 43138428, user_id: 724, amountInCents: 4000, fullname: 'Luisa Lane', firstName: 'Luisa', lastName: 'Lane', avatar: { src: `${baseUrl}img/donor-5.jpg` }, createdAt: 1536504944000, public: true, },
-          { fundraiser_id: 254, nonprofit_ein: 43138428, user_id: 725, amountInCents: 2500, fullname: 'Martin Fitzgerald', firstName: 'Martin', lastName: 'Fitzgerald', User: {avatar: { src: `${baseUrl}img/donor-6.jpg` }}, createdAt: 1516504944000, public: true, },
-          { fundraiser_id: 254, nonprofit_ein: 43138428, user_id: 726, amountInCents: 2500, fullname: 'Sandra Martignon', firstName: 'Sandra', lastName: 'Martignon', User: {avatar: { src: `${baseUrl}img/donor-10.jpg` }}, createdAt: 1540504944000, public: true, },
+          { fundraiser_id: 254, nonprofit_ein: 237069110, user_id: 721, amountInCents: 4500, User: { firstName: "Francisca", lastName: "Morana", nickname: "Fran", avatar: { src: `${baseUrl}img/donor-1.jpg` }}, fullname: 'Francisca Morana', firstName: 'Francisca', lastName: 'Morana', avatar: `${baseUrl}img/donor-1.jpg`, createdAt: 1521504544000, public: true, },
+          { fundraiser_id: 254, nonprofit_ein: 237069110, user_id: 722, amountInCents: 3000, fullname: 'Ariadna Beckenbauer', firstName: 'Ariadna', lastName: 'Beckenbauer', createdAt: 1526504544000, public: true, },
+          { fundraiser_id: 254, nonprofit_ein: 237069110, user_id: 723, amountInCents: 3500, fullname: 'Andreas Higgins', firstName: 'Andreas', lastName: 'Higgins', User: { avatar: { src: `${baseUrl}img/donor-4.jpg` } }, createdAt: 1539504944000, public: true, },
+          { fundraiser_id: 254, nonprofit_ein: 237069110, user_id: 724, amountInCents: 4000, fullname: 'Luisa Lane', firstName: 'Luisa', lastName: 'Lane', avatar: { src: `${baseUrl}img/donor-5.jpg` }, createdAt: 1536504944000, public: true, },
+          { fundraiser_id: 254, nonprofit_ein: 237069110, user_id: 725, amountInCents: 2500, fullname: 'Martin Fitzgerald', firstName: 'Martin', lastName: 'Fitzgerald', User: {avatar: { src: `${baseUrl}img/donor-6.jpg` }}, createdAt: 1516504944000, public: true, },
+          { fundraiser_id: 254, nonprofit_ein: 237069110, user_id: 726, amountInCents: 2500, fullname: 'Sandra Martignon', firstName: 'Sandra', lastName: 'Martignon', User: {avatar: { src: `${baseUrl}img/donor-10.jpg` }}, createdAt: 1540504944000, public: true, },
         ]
       },
       byDate: {
         data: [
-          { fundraiser_id: 254, nonprofit_ein: 43138428, user_id: 721, amountInCents: 4500, User: { firstName: "Francisca", lastName: "Morana", nickname: "Fran", avatar: { src: `${baseUrl}img/donor-1.jpg` }}, fullname: 'Francisca Morana', firstName: 'Francisca', lastName: 'Morana', avatar: `${baseUrl}img/donor-1.jpg`, createdAt: 1521504544000, public: true, },
-          { fundraiser_id: 254, nonprofit_ein: 43138428, user_id: 722, amountInCents: 3000, fullname: 'Ariadna Beckenbauer', firstName: 'Ariadna', lastName: 'Beckenbauer', createdAt: 1526504544000, public: true, },
-          { fundraiser_id: 254, nonprofit_ein: 43138428, user_id: 723, amountInCents: 3500, fullname: 'Andreas Higgins', firstName: 'Andreas', lastName: 'Higgins', avatar: { src: `${baseUrl}img/donor-4.jpg` }, createdAt: 1539504944000, public: true, },
-          { fundraiser_id: 254, nonprofit_ein: 43138428, user_id: 724, amountInCents: 4000, fullname: 'Luisa Lane', firstName: 'Luisa', lastName: 'Lane', avatar: { src: `${baseUrl}img/donor-5.jpg` }, createdAt: 1536504944000, public: true, },
-          { fundraiser_id: 254, nonprofit_ein: 43138428, user_id: 725, amountInCents: 2500, fullname: 'Martin Fitzgerald', firstName: 'Martin', lastName: 'Fitzgerald', User: {avatar: { src: `${baseUrl}img/donor-6.jpg` }}, createdAt: 1516504944000, public: true, },
-          { fundraiser_id: 254, nonprofit_ein: 43138428, user_id: 726, amountInCents: 2500, fullname: 'Sandra Martignon', firstName: 'Sandra', lastName: 'Martignon', User: {avatar: { src: `${baseUrl}img/donor-10.jpg` }}, createdAt: 1540504944000, public: true, },
+          { fundraiser_id: 254, nonprofit_ein: 237069110, user_id: 721, amountInCents: 4500, User: { firstName: "Francisca", lastName: "Morana", nickname: "Fran", avatar: { src: `${baseUrl}img/donor-1.jpg` }}, fullname: 'Francisca Morana', firstName: 'Francisca', lastName: 'Morana', avatar: `${baseUrl}img/donor-1.jpg`, createdAt: 1521504544000, public: true, },
+          { fundraiser_id: 254, nonprofit_ein: 237069110, user_id: 722, amountInCents: 3000, fullname: 'Ariadna Beckenbauer', firstName: 'Ariadna', lastName: 'Beckenbauer', createdAt: 1526504544000, public: true, },
+          { fundraiser_id: 254, nonprofit_ein: 237069110, user_id: 723, amountInCents: 3500, fullname: 'Andreas Higgins', firstName: 'Andreas', lastName: 'Higgins', avatar: { src: `${baseUrl}img/donor-4.jpg` }, createdAt: 1539504944000, public: true, },
+          { fundraiser_id: 254, nonprofit_ein: 237069110, user_id: 724, amountInCents: 4000, fullname: 'Luisa Lane', firstName: 'Luisa', lastName: 'Lane', avatar: { src: `${baseUrl}img/donor-5.jpg` }, createdAt: 1536504944000, public: true, },
+          { fundraiser_id: 254, nonprofit_ein: 237069110, user_id: 725, amountInCents: 2500, fullname: 'Martin Fitzgerald', firstName: 'Martin', lastName: 'Fitzgerald', User: {avatar: { src: `${baseUrl}img/donor-6.jpg` }}, createdAt: 1516504944000, public: true, },
+          { fundraiser_id: 254, nonprofit_ein: 237069110, user_id: 726, amountInCents: 2500, fullname: 'Sandra Martignon', firstName: 'Sandra', lastName: 'Martignon', User: {avatar: { src: `${baseUrl}img/donor-10.jpg` }}, createdAt: 1540504944000, public: true, },
         ]
       },
       topFundraisers: {
         data: [
-          { fundraiser_id: 254, nonprofit_ein: 43138428, user_id: 721, amountInCents: 214500, User: { firstName: "Francisca", lastName: "Morana", nickname: "Fran", avatar: { src: `${baseUrl}img/donor-1.jpg` }}, fullname: 'Francisca Morana', firstName: 'Francisca', lastName: 'Morana', avatar: `${baseUrl}img/donor-1.jpg`, createdAt: 1521504544000, public: true, },
-          { fundraiser_id: 254, nonprofit_ein: 43138428, user_id: 723, amountInCents: 233500, fullname: 'Andreas Higgins', firstName: 'Andreas', lastName: 'Higgins', User: { avatar: { src: `${baseUrl}img/donor-4.jpg` } }, createdAt: 1539504944000, public: true, },
-          { fundraiser_id: 254, nonprofit_ein: 43138428, user_id: 722, amountInCents: 330000, fullname: 'Ariadna Beckenbauer', firstName: 'Ariadna', lastName: 'Beckenbauer', createdAt: 1526504544000, public: true, },
-          { fundraiser_id: 254, nonprofit_ein: 43138428, user_id: 725, amountInCents: 132500, fullname: 'Martin Fitzgerald', firstName: 'Martin', lastName: 'Fitzgerald', User: {avatar: { src: `${baseUrl}img/donor-6.jpg` }}, createdAt: 1516504944000, public: true, },
-          { fundraiser_id: 254, nonprofit_ein: 43138428, user_id: 726, amountInCents: 215000, fullname: 'Sandra Martignon', firstName: 'Sandra', lastName: 'Martignon', User: {avatar: { src: `${baseUrl}img/donor-10.jpg` }}, createdAt: 1540504944000, public: true, },
-          { fundraiser_id: 254, nonprofit_ein: 43138428, user_id: 724, amountInCents: 114000, fullname: 'Luisa Lane', firstName: 'Luisa', lastName: 'Lane', avatar: { src: `${baseUrl}img/donor-5.jpg` }, createdAt: 1536504944000, public: true, },
+          { fundraiser_id: 254, nonprofit_ein: 237069110, user_id: 721, amountInCents: 214500, User: { firstName: "Francisca", lastName: "Morana", nickname: "Fran", avatar: { src: `${baseUrl}img/donor-1.jpg` }}, fullname: 'Francisca Morana', firstName: 'Francisca', lastName: 'Morana', avatar: `${baseUrl}img/donor-1.jpg`, createdAt: 1521504544000, public: true, },
+          { fundraiser_id: 254, nonprofit_ein: 237069110, user_id: 723, amountInCents: 233500, fullname: 'Andreas Higgins', firstName: 'Andreas', lastName: 'Higgins', User: { avatar: { src: `${baseUrl}img/donor-4.jpg` } }, createdAt: 1539504944000, public: true, },
+          { fundraiser_id: 254, nonprofit_ein: 237069110, user_id: 722, amountInCents: 330000, fullname: 'Ariadna Beckenbauer', firstName: 'Ariadna', lastName: 'Beckenbauer', createdAt: 1526504544000, public: true, },
+          { fundraiser_id: 254, nonprofit_ein: 237069110, user_id: 725, amountInCents: 132500, fullname: 'Martin Fitzgerald', firstName: 'Martin', lastName: 'Fitzgerald', User: {avatar: { src: `${baseUrl}img/donor-6.jpg` }}, createdAt: 1516504944000, public: true, },
+          { fundraiser_id: 254, nonprofit_ein: 237069110, user_id: 726, amountInCents: 215000, fullname: 'Sandra Martignon', firstName: 'Sandra', lastName: 'Martignon', User: {avatar: { src: `${baseUrl}img/donor-10.jpg` }}, createdAt: 1540504944000, public: true, },
+          { fundraiser_id: 254, nonprofit_ein: 237069110, user_id: 724, amountInCents: 114000, fullname: 'Luisa Lane', firstName: 'Luisa', lastName: 'Lane', avatar: { src: `${baseUrl}img/donor-5.jpg` }, createdAt: 1536504944000, public: true, },
         ]
       },
       current: 1,
@@ -558,7 +558,7 @@ export default new Vuex.Store({
       data: [
         {
           fundraiser_id: 254,
-          nonprofit_ein: 43138428,
+          nonprofit_ein: 237069110,
           user_id: 721,
           amount: 625,
           fullname: 'Francisca Morana',
@@ -570,7 +570,7 @@ export default new Vuex.Store({
         },
         {
           fundraiser_id: 254,
-          nonprofit_ein: 43138428,
+          nonprofit_ein: 237069110,
           user_id: 722,
           amount: 550,
           fullname: 'Ariadna Beckenbauer',
@@ -582,7 +582,7 @@ export default new Vuex.Store({
         },
         {
           fundraiser_id: 254,
-          nonprofit_ein: 43138428,
+          nonprofit_ein: 237069110,
           user_id: 723,
           amount: 400,
           fullname: 'Andreas Higgins',
@@ -594,7 +594,7 @@ export default new Vuex.Store({
         },
         {
           fundraiser_id: 254,
-          nonprofit_ein: 43138428,
+          nonprofit_ein: 237069110,
           user_id: 724,
           amount: 350,
           fullname: 'Luisa Lane',
@@ -606,7 +606,7 @@ export default new Vuex.Store({
         },
         {
           fundraiser_id: 254,
-          nonprofit_ein: 43138428,
+          nonprofit_ein: 237069110,
           user_id: 725,
           amount: 220,
           fullname: 'Martin Fitzgerald',
@@ -618,7 +618,7 @@ export default new Vuex.Store({
         },
         {
           fundraiser_id: 254,
-          nonprofit_ein: 43138428,
+          nonprofit_ein: 237069110,
           user_id: 726,
           amount: 180,
           fullname: 'Sandra Martignon',
