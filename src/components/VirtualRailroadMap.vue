b25bb4526ce1069bfc046555b96b5480a8e1180e
John Vee Uminga <johnveeuminga@gmail.com>
2019-01-21 14:38:12 +0800

    Moved leegend from VirtualRailroadMap component to HomePage view.

--- src/components/VirtualRailroadMap.vue
+++ src/components/VirtualRailroadMap.vue
@@ -1,7 +1,6 @@
 <template>
-	<section class="section virtual-railroad-map-section">
+	<div class="virtual-railroad-map">
 		<div class="container">
-			<p class='has-text-centered virtual-railroad-map__intro'>Click the bulbs to see who's helping free slaves and where they're helping. Click <a href='#'>here </a> to be part of the Virtual Railroad.</p>
 			<GmapMap
 				class='map'
 				:center='{lat: 32.33888927939217, lng: 6.1015625}'
@@ -19,7 +18,7 @@
 					v-for='(user, index) in users'
 					:key='index'
 					:position='{lat: parseFloat(user.latitude), lng: parseFloat(user.longitude)}'
-					:icon='require("@/assets/img/light_bulb_16.png")'
+					:icon='require("@/assets/img/light_bulb_16.png")'ue
 					@click='setSelectedUser(user)'
 				/>
 				<GmapMarker
@@ -50,17 +49,7 @@
 		>
 			<p class='has-text-centered is-marginless'>Join <a href="#">now</a> to help free slaves, and turn your red marker into a lamp of freedom!</p>	
 		</div>
-		<div class='legends'>
-			<div class='container'>
-				<p>
-					<img src='@/assets/img/light_bulb_32.png' alt='Light Bulb'><span class='has-text-weight-bold'> = person helping to free slaves. </span> Individuals helping the Underground Railroad left lit lamps hanging on their barn doors and sheds overnight to let runaway slaves know they were safe havens where they could sleep or find food and water for their journey. They also used lamps to signal to runaway slaves who were preparing to cross rivers whether there was anyone on the other shore who would report them or try to capture them, or if the coast was clear to swim across.
-				</p>
-				<p>
-					<img src='@/assets/img/star_32.png' alt='Star'><span class='has-text-weight-bold'> = nonprofit helping to free slaves.</span> In the 1800s, slaves trying to escape north to freedom regularly relied on the North star as a beacon and compass because it's the only star in the sky that doesn't change its location. Today, slaves rely on nonprofits to help them gain their freedom and the nonprofits are steadfast in their commitment to free slaves, so we represent them with stars.
-				</p>
-			</div>
-		</div>
-	</section>
+	</div>
 </template>
 
 <script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,15 +2,28 @@
   <div class="layout-base">
     <intro-video />
     <app-header />
+		<section class='home-virtual-railroad-map section'>	
+			<div class='container'>
+			<p class='has-text-centered home-virtual-railroad-map__intro'>Click the bulbs to see who's helping free slaves and where they're helping. Click <a href='#'>here </a> to be part of the Virtual Railroad.</p>
+				<home-map />
+				<div class='legends'>
+					<p>
+						<img src='@/assets/img/light_bulb_32.png' alt='Light Bulb'><span class='has-text-weight-bold'> = person helping to free slaves. </span> Individuals helping the Underground Railroad left lit lamps hanging on their barn doors and sheds overnight to let runaway slaves know they were safe havens where they could sleep or find food and water for their journey. They also used lamps to signal to runaway slaves who were preparing to cross rivers whether there was anyone on the other shore who would report them or try to capture them, or if the coast was clear to swim across.
+					</p>
+					<p>
+						<img src='@/assets/img/star_32.png' alt='Star'><span class='has-text-weight-bold'> = nonprofit helping to free slaves.</span> In the 1800s, slaves trying to escape north to freedom regularly relied on the North star as a beacon and compass because it's the only star in the sky that doesn't change its location. Today, slaves rely on nonprofits to help them gain their freedom and the nonprofits are steadfast in their commitment to free slaves, so we represent them with stars.
+					</p>
+				</div>
+			</div>
+		</section>
 		<virtual-railroad-map />
 		<shared-footer />
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-
 import AppHeader from 'LocalComponents/AppHeader'
+import HomeMap from 'LocalComponents/HomeMap'
 import VirtualRailroadMap from 'LocalComponents/VirtualRailroadMap'
 import IntroVideo from 'LocalComponents/IntroVideo'
 import SharedFooter from 'Components/Shared/SharedFooter'
@@ -21,6 +34,7 @@ export default {
   components: {
     AppHeader,
 		VirtualRailroadMap,
+		HomeMap,
     IntroVideo,
     SharedFooter,
  	},
@@ -37,11 +51,13 @@ export default {
       ],
     };
   },
-
-  computed: {
-    ...mapState({
-      isPlaying: state => state.video.isPlaying,
-    })
-  },
 };
 </script>
+
+<style lang='scss'scoped>
+.legends {
+	img {
+		vertical-align: middle;
+	}
+}	
+</style>
