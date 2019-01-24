4e42904b13acb9f4ce636871b7edf3abacd1af92
John Vee Uminga <johnveeuminga@gmail.com>
2019-01-24 22:24:26 +0800

    Added UserEditSupportedNonprofits, UserEditForm component.

--- src/components/User/Edit/UserEditForm.vue
+++ src/components/User/Edit/UserEditForm.vue
@@ -1,11 +1,62 @@
 <template>
-	<div class='user-edit-form'>
-		This is the user edit form.
+	<div class='user-edit-form__wrapper columns'>
+		<div class='user-edit-avatar__wrapper column is-3'>
+			<user-edit-avatar 
+				:user='user'	
+				@avatarChange='onAvatarChange'
+			/>
+		</div>
+		<div class='column'>
+			<h4 class='user-profile__heading user-profile__heading--underline'>User Details</h4>
+			<user-edit-details 
+				:user='user'	
+			/>	
+		</div>
+		<div class='column is-4'>
+			<h4 class='user-profile__heading user-profile__heading--underline'>Nonprofits I Support</h4>
+			<user-edit-supported-nonprofits 
+				:user='user'	
+			/>
+		</div>
 	</div>
 </template>
 
 <script>
+import UserEditAvatar from 'LocalComponents/User/Edit/UserEditAvatar'
+import UserEditDetails from 'LocalComponents/User/Edit/UserEditDetails'
+import UserEditSupportedNonprofits from 'LocalComponents/User/Edit/UserEditSupportedNonprofits'
+
 export default {
-	name: 'UserEditForm',
+  name: 'UserEditForm',
+
+	components: {
+		UserEditAvatar,
+		UserEditDetails,
+		UserEditSupportedNonprofits,
+	},
+
+	props: {
+		user: {
+			type: Object,
+			required: true,
+		},
+	},
+
+	data () {
+		return {
+			userData: this.user,
+		}	
+	},
+
+	methods: {
+		/** 
+		 * Event handler for on avatar change.
+		 *
+		 * @param {Object} file
+		 */
+		onAvatarChange (file) {
+			this.userData.picture = file.url
+		},
+	},
 }
 </script>
---
+++ src/components/User/Edit/UserEditSupportedNonprofits.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class='user-edit-supported-nonprofits'>
+		<user-supported-nonprofits 
+			:user='user'	
+		/>
+		<div class='user-edit-supported-nonprofits-additional'>
+			<ol class='user-edit-supported-nonprofits-list'>
+				<li class='user-edit-supported-nonprofits-list__item' v-for='n in inputCount'>
+					<select-nonprofits 
+						:id='`select-nonprofit-${n}`' 
+						v-model='nonprofits[n]'
+						:exclude='user.nonprofits'
+					/>	
+				</li>
+			</ol>
+		</div>
+	</div>
+</template>
+
+<script>
+import UserSupportedNonprofits from 'LocalComponents/User/UserSupportedNonprofits'
+import SelectNonprofits from 'LocalComponents/Form/SelectNonprofits'
+
+export default {
+	name: 'UserEditSupportedNonprofits',
+
+	components: {
+		UserSupportedNonprofits,
+		SelectNonprofits,
+	},
+
+	props: {
+		user: {
+			type: Object,
+			required: false,
+		},
+	},
+
+	data () {
+		return {
+			inputCount: 3,
+			nonprofits: [],
+		}
+	},
+}
+</script>
+
+<style lang='scss' scoped>
+.user-edit-supported-nonprofits-list {
+	&__item {
+		margin-top: 1em;
+	}
+}
+</style>
