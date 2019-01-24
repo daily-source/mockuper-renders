76e84297af0f646f8fdf07d766e220b5337b29d1
John Vee Uminga <johnveeuminga@gmail.com>
2019-01-24 22:22:50 +0800

    Added UserAvatar Component.

---
+++ src/components/User/UserAvatar.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class='user-avatar'>
+		<img 
+		 :src='url'
+		 :alt='alt'
+		 :class='["user-avatar__img is-fullwidth", ...imgClass]'
+			@click='onClick'
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'UserAvatar',
+
+	props: {
+		url: {
+			type: String,
+			required: true,
+		},
+
+		alt: {
+			type: String,
+			required: false,
+		},
+
+		imgClass: {
+			type: Array,
+			required: false,
+			default: () => {
+				return []
+			},
+		},
+	},
+
+	methods: {
+		/** 
+		 * Emits on click when the image is clicked.
+		 */
+		onClick () {
+			this.$emit('click')
+		},
+	},
+}
+</script>
+
+<style lang='scss' scoped>
+.user-avatar {
+	&__img {
+		display: block;
+		width: 100%;
+	}
+}
+</style>
