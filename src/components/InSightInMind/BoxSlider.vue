cc823743c15cc11bd9ad7e4a7f94ba0487b90ad8
John Vee Uminga <johnveeuminga@gmail.com>
2019-02-05 18:25:27 +0800

    Added CTA slide.

--- src/components/InSightInMind/BoxSlider.vue
+++ src/components/InSightInMind/BoxSlider.vue
@@ -20,6 +20,14 @@
 		</transition>
     <transition name='box-fade'>
       <div 
+        class='box-slider__slide box-slider__slide--cta'
+        v-if='showCta'
+      >
+        <h2 class='has-text-weight-bold'>Please Help Now</h2> 
+      </div>
+    </transition>
+    <transition name='box-fade'>
+      <div 
       class="box-slider__hover-box-wrap"
       v-if='hovered'>
         <div class="box-slider__hover-box">
@@ -80,9 +88,11 @@ export default {
     return {
       hovered: false,
       dataShown: false,
-      imageShowDuration: 1600,
+      imageShowDuration: 1200,
+      ctaDuration: 800,
       selectedImageIndex: 1,
-			showImage: false,
+      showImage: false,
+      showCta: false,
     }
   },
 
@@ -92,13 +102,23 @@ export default {
 
   methods: {
 		playSlider () {
-			const timeoutDur = this.showImage ? this.imageShowDuration : this.imageSlideDelay
-
 			setTimeout(() => {
-				this.showImage = !this.showImage
-				this.setSelectedImageIndex()
+        if (!this.showCta && !this.showImage) {
+          this.showImage = true
+        } else {
+          if (this.showCta) {
+            this.showCta = false
+          }
+
+          if (this.showImage) {
+            this.showCta = true
+            this.showImage = false
+            this.setSelectedImageIndex()
+          }
+        }
+
 				this.playSlider()
-			}, timeoutDur)
+			}, this.timeoutDur)
 		},
 
     toggleHovered () {
@@ -147,119 +167,153 @@ export default {
 
 		selectedImage () {
 			return this.imagesSrc[this.selectedImageIndex - 1]
-		},
+    },
+    
+    timeoutDur () {
+      let timeoutDur = this.imageSlideDelay - (this.ctaDuration + this.imageShowDuration)
+
+      if (this.showCta) {
+        timeoutDur = this.ctaDuration
+      }
+
+      if (this.showImage) {
+        timeoutDur = this.imageShowDuration
+      }
+
+      return timeoutDur
+    }
   },
 }
 </script>
 
-<style lang='scss'>
-  .box-slider {
-    border-radius: $box-slider-border-radius;
-    text-align: center;
-    height: $box-slider-height;
-    position: relative;
+<style lang='scss' scoped>
+.box-slider {
+  border-radius: $box-slider-border-radius;
+  text-align: center;
+  height: $box-slider-height;
+  position: relative;
 
-		@include breakpoint($mobile) {
-			height: 250px;
-		}
+  @include breakpoint($mobile) {
+    height: 250px;
+  }
+}
+
+.box-slider__slide {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: $box-slider-height;
+  border-radius: $box-slider-border-radius;
+  overflow: hidden;
+  background-image: linear-gradient(135deg, $primary 0%, $color-primary-tint 90%);
+
+  @include breakpoint($mobile) {
+    height: 250px;	
   }
 
-  .box-slider__slide {
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: $box-slider-height;
-    border-radius: $box-slider-border-radius;
-    overflow: hidden;
-    background-image: linear-gradient(135deg, $primary 0%, $color-primary-tint 90%);
-
-		@include breakpoint($mobile) {
-			height: 250px;	
-		}
-
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-      display: block;
-    }
-
-		&--image {
-			position: absolute;
-			top: 0;
-			left: 0;
-			right: 0;
-			height: 220px;
-	
-			@include breakpoint($mobile) {
-				height: 250px;	
-			}		
-		}
-  }
-
-  .box-slider-title__heading {
-    max-width: 70%;
-    margin-left: auto;
-    margin-right: auto;
-    color: #fff;
-    font-weight: 700;
-    font-size: 1.5em;
-    font-family: $font-family-base;
-    margin-bottom: 0;
-
-		@include breakpoint($mobile) {
-			font-size: 1.25em;
-		}
-  }
-
-  .box-slider__hover-box {
+  img {
     width: 100%;
     height: 100%;
-    background-color: $secondary;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    display: flex;
-    color: #fff;
-    padding: .875em;
-    border-radius: $box-slider-border-radius;
-
-    h3 {
-      font-family: $font-family-base;
-      color: #fff;
-      font-size: 1.25rem;
-    }
-
-    p {
-      font-size: 1rem;
-      &:last-child {
-        margin-bottom: 0;
-      }
-    }
+    object-fit: cover;
+    display: block;
   }
 
-  .box-slider__hover-box-wrap {
+  &--image {
     position: absolute;
     top: 0;
     left: 0;
-    height: 100%;
-    width: 100%;
+    right: 0;
+    height: 220px;
+
+    @include breakpoint($mobile) {
+      height: 250px;	
+    }		
   }
 
-  // Fade animation
-  .box-fade-enter,
-  .box-fade-leave-to {
-    opacity: 0;
+  &--cta {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    background-color: $danger;
+    background-image: none;
+    height: 220px;
+
+    h2 {
+      color: #fff;
+      margin-bottom: 0;
+      font-size: 1.5em;
+    }
+
+    @include breakpoint($mobile) {
+      height: 250px;	
+    }	
+  }
+}
+
+.box-slider-title__heading {
+  max-width: 70%;
+  margin-left: auto;
+  margin-right: auto;
+  color: #fff;
+  font-weight: 700;
+  font-size: 1.5em;
+  font-family: $font-family-base;
+  margin-bottom: 0;
+
+  @include breakpoint($mobile) {
+    font-size: 1.25em;
+  }
+}
+
+.box-slider__hover-box {
+  width: 100%;
+  height: 100%;
+  background-color: $secondary;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  display: flex;
+  color: #fff;
+  padding: .875em;
+  border-radius: $box-slider-border-radius;
+
+  h3 {
+    font-family: $font-family-base;
+    color: #fff;
+    font-size: 1.25rem;
   }
 
-  .box-fade-enter-active,
-  .box-fade-leave-active {
-    transition: opacity .2s ease-out;
+  p {
+    font-size: 1rem;
+    &:last-child {
+      margin-bottom: 0;
+    }
   }
+}
 
-  .box-fade-enter-to,
-  .box-fade-enter-leave {
-    opacity: 1;
-  }
+.box-slider__hover-box-wrap {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+
+// Fade animation
+.box-fade-enter,
+.box-fade-leave-to {
+  opacity: 0;
+}
+
+.box-fade-enter-active,
+.box-fade-leave-active {
+  transition: opacity .2s ease-out;
+}
+
+.box-fade-enter-to,
+.box-fade-enter-leave {
+  opacity: 1;
+}
 </style>
