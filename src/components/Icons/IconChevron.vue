1cc7f986151f2caaccc034a906c1ba3491a4a51d
John Vee Uminga <johnveeuminga@gmail.com>
2019-02-14 20:53:25 +0800

    Fixed Firefox issues with svgs.

--- src/components/Icons/IconChevron.vue
+++ src/components/Icons/IconChevron.vue
@@ -1,7 +1,8 @@
 <template>
   <svg 
     xmlns="http://www.w3.org/2000/svg" 
-    :style='{width: width, height: height}'
+    :width='width'
+    :height='height'
     viewBox="0 0 24 24"
   >
     <g id="Group_11" data-name="Group 11" transform="translate(-1264 -763)">
--- src/components/Icons/IconInfo.vue
+++ src/components/Icons/IconInfo.vue
@@ -1,7 +1,8 @@
 <template>
   <svg 
     xmlns="http://www.w3.org/2000/svg" 
-    :style='{width: width, height: height}'
+    :width='width'
+    :height='height'
     viewBox="0 0 24 24"
   >
     <g id="iconfinder-icon" transform="translate(1 8)">
