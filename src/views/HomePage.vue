7bef275a3d203846a86cb844181df328409b9790
John Vee Uminga <johnveeuminga@gmail.com>
2019-03-09 16:32:45 +0800

    Home page fixes.

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="layout-base">
 		<app-header />
-    <section class='section' style='height: calc(100vh - 105px); display: flex; align-items: center; justify-content: center;'>
-      <h1>Website is currently under construction...</h1>
+    <app-banner />
+    <section class='section'>
+      <app-description />
+    </section>
+    <section class='site-content'>
+      <nonprofit-form />
+    </section>
+    <section class='site-sample-nonprofits'>
+      <nonprofit-fundraisers
+        :fundraisers='fundraisers'
+      >
+        <h2 class='has-text-centered' slot='heading'>Examples of Loseathons</h2>
+      </nonprofit-fundraisers>
     </section>
     <shared-footer />
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 import AppHeader from 'Components/Loseathon/AppHeader'
+import AppBanner from 'Components/Loseathon/AppBanner'
+import NonprofitForm from 'LocalComponents/Loseathon/NonprofitForm'
+import NonprofitFundraisers from 'Components/nonprofit/NonprofitFundraisers'
+import AppDescription from 'LocalComponents/Loseathon/AppDescription'
 import SharedFooter from 'Components/Shared/SharedFooter'
 
 export default {
@@ -17,6 +34,10 @@ export default {
 
   components: {
     AppHeader,
+    AppBanner,
+    AppDescription,
+    NonprofitForm,
+    NonprofitFundraisers,
     SharedFooter,
   },
 
@@ -24,13 +45,19 @@ export default {
     const description = 'A Loseathon is similar to a walkathon but instead of walking a certain distance to raise money, a person loses a certain amount of weight';
     const title = 'Raise money while losing weight!';
     return {
-      title: 'Home - v1',
+      title: 'Home',
       meta: [
         { vmid: 'description', name: 'description', content: description },
         { vmid: 'og:title', property: 'og:title', content: title },
         { vmid: 'og:description', name: 'og:description', content: description },
       ],
     };
+  },
+  computed: {
+    ...mapState({
+      nonprofit: state => state.nonprofit[this.ein],
+      fundraisers: state => state.fundraisers.data,
+    }),
   },
 
 };
