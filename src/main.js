618184b0ca5bb65feb8c5284041747164e79266f
John Vee Uminga <johnveeuminga@example.com>
2019-04-06 19:18:16 +0800

    Daily Indulgence Drop initial template.

--- src/main.js
+++ src/main.js
@@ -24,15 +24,15 @@ Object.keys(filters).forEach((key) => {
 Vue.use(SmoothScroll);
 
 const bgImages = [
-  'banner-img-1.jpg',
-  'banner-img-2.jpg',
-  'banner-img-3.jpg',
-  'banner-img-4.jpg',
-  'banner-img-5.jpg',
-  'banner-img-6.jpg',
+  'banner-photo-1.jpg',
+  'banner-photo-2.jpg',
+  'banner-photo-3.jpg',
+  'banner-photo-4.jpg',
+  'banner-photo-5.jpg',
+  'banner-photo-6.jpg',
 ]
 
-Vue.use(BannerSwitcher, store, {bgImages, selected: 0})
+Vue.use(BannerSwitcher, store, {bgImages, selected: 1})
 
 new Vue({
   router,
