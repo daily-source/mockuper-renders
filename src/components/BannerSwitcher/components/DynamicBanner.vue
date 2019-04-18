117de1a95b76b2085796fbac804bc902fb536555
John Vee Uminga <johnveeuminga@example.com>
2019-04-18 19:04:56 +0800

    5050raffle initial template.

--- src/components/BannerSwitcher/components/DynamicBanner.vue
+++ src/components/BannerSwitcher/components/DynamicBanner.vue
@@ -39,7 +39,7 @@ export default {
 
 <style lang="scss" scoped>
   #banner {
-    min-height: 400px;
+    min-height: 600px;
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
