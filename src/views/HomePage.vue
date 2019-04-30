4ea1b7b6658468070940d33444a668952b9a12df
John Vee Uminga <johnveeuminga@example.com>
2019-04-30 15:32:46 +0800

    v5 changes.

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -79,7 +79,7 @@ export default {
     var description = "Double the results, half the effort. A Volunteerathon® lets you make a far greater impact with your time than traditional fundraising events."
     var title = "Create a volunteerathon and do good!"
     return {
-      title: "Bridges to God - v3",
+      title: "Bridges to God - v5",
       meta: [
         { vmid: "description", name: "description", content: description },
         { vmid: "og:title", property: "og:title", content: title },
