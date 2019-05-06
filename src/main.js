55f9960cd9f5c5fecd307bfc9d2795bbbba6c9a1
John Vee Uminga <johnveeuminga@example.com>
2019-05-07 00:57:14 +0800

    v2 changes.

--- src/main.js
+++ src/main.js
@@ -25,14 +25,14 @@ Vue.use(SmoothScroll);
 
 const bgImages = [
   'banner-photo-1.jpg',
-  'banner-photo-2.jpg',
-  'banner-photo-3.jpg',
-  'banner-photo-4.jpg',
+  // 'banner-photo-2.jpg',
+  // 'banner-photo-3.jpg',
+  // 'banner-photo-4.jpg',
   'banner-photo-5.jpg',
-  'banner-photo-6.jpg',
+  // 'banner-photo-6.jpg',
 ]
 
-Vue.use(BannerSwitcher, store, {bgImages, selected: 4})
+Vue.use(BannerSwitcher, store, {bgImages, selected: 1})
 
 new Vue({
   router,
