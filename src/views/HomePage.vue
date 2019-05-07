3ea6d86605a3418f49e15fad10d679ad683e2b3d
John Vee Uminga <johnveeuminga@example.com>
2019-05-07 15:06:58 +0800

    Homepage revisions.

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -11,10 +11,10 @@
     <div class='instructions'>
       <div class='container'>
         <p>
-          <span class='has-text-weight-bold'>WEB DEVELOPER:</span> when the fundraising activity section is completed at the nonprofit profile page, normally you should copy it here so that users can start creating their fundraiser from the front page. For some websites, we don’t use that approach. But normally we do.   
+          WEB DEVELOPER: when the fundraising activity section is completed at the nonprofit profile page, normally you should copy it here so that users can start creating their fundraiser from the front page. For some websites, we don’t use that approach. But normally we do.   
         </p>
         <p>
-          <span class='has-text-weight-bold'>ALSO:</span> often a short piece of text in put inside of the banner photo.
+          ALSO: often a short piece of text in put inside of the banner photo.
         </p>
       </div>
     </div>
@@ -74,7 +74,7 @@ export default {
     var description = "Double the results, half the effort. A Volunteerathon® lets you make a far greater impact with your time than traditional fundraising events."
     var title = "Create a volunteerathon and do good!"
     return {
-      title: "Dollars for Dares - Home",
+      title: "Dollars for Dares - v1",
       meta: [
         { vmid: "description", name: "description", content: description },
         { vmid: "og:title", property: "og:title", content: title },
