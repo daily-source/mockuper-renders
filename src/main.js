4db6298315de0a7b9d66868acbca40101557095e
John Vee Uminga <johnveeuminga@example.com>
2019-05-15 12:18:16 +0800

    v2 initial template.

--- src/main.js
+++ src/main.js
@@ -26,10 +26,7 @@ Vue.use(SmoothScroll);
 const bgImages = [
   'banner-photo-1.jpg',
   'banner-photo-2.jpg',
-  'banner-photo-3.jpg',
   'banner-photo-4.jpg',
-  'banner-photo-5.jpg',
-  'banner-photo-6.jpg',
 ]
 
 Vue.use(BannerSwitcher, store, {bgImages, selected: 0})
