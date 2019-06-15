0240da018cd13e0cc4b17cb9a8b13ccf7e30c799
John Vee Uminga <johnveeuminga@example.com>
2019-06-15 20:49:08 +0800

    General changes.

--- src/components/Nonprofit/Register/NonprofitRegisterFormDetails.vue
+++ src/components/Nonprofit/Register/NonprofitRegisterFormDetails.vue
@@ -22,27 +22,6 @@
       </div>
     </div>
     <div class='field'>
-      <label for='description' class='label'>Description <span class='has-text-danger'>*</span>:</label>
-      <div class='control'>
-        <textarea-with-warning
-          class='nonprofit-register-form-details__textarea'
-          name='description'
-          :max-length='descriptionMaxLength'
-          id='description' 
-          v-model='form.description'
-          @invalid='(errors) => onFieldError("description", errors)'
-        />
-        <div class="field-errors">
-          <p 
-            class='help has-text-danger has-text-weight-bold'
-            v-if='errors.description && errors.description.maxLength'
-          >
-            * Description cannot exceed {{ descriptionMaxLength }} characters. Description currently exceeds {{ form.description.length - descriptionMaxLength }} character(s).
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class='field'>
       <label for='link' class='label'>Website Link <span class='has-text-danger'>*</span>:</label>
       <div class='control'>
         <!-- <input class='input' type='text' name='link' id='link' v-model='form.url'> -->
@@ -60,6 +39,36 @@
             v-if='errors.url && errors.url.maxLength'
           >
             * URL cannot exceed {{ 50 }} characters. Name currently exceeds {{ form.name.length - 50 }} character(s).
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class='field'>
+      <label for='description' class='label'>Description <span class='has-text-danger'>*</span>:</label>
+      <div class='control'>
+        <textarea-with-warning
+          class='nonprofit-register-form-details__textarea'
+          name='description'
+          placeholder='Maximum of 500 characters '
+          id='description'
+          rows='9'
+          :max-length='descriptionMaxLength'
+          :warning-max-length='475'
+          v-model='form.description'
+          @invalid='(errors) => onFieldError("description", errors)'
+          @warningChange='(warnings) => onFieldWarning("description", warnings)'
+        />
+        <div class="field-errors">
+          <p 
+            class='help has-text-success has-text-weight-bold'
+            v-if='(errors.description && errors.description.maxLength) || (warnings.description && warnings.description.maxLength)'
+          >
+            Maximum {{ descriptionMaxLength }} characters. Remaining: 
+            <span
+              :class='{"has-text-danger": errors.description && errors.description.maxLength}'
+            >
+              {{ descriptionMaxLength - form.description.length }}
+            </span>
           </p>
         </div>
       </div>
@@ -104,6 +113,7 @@ export default {
       descriptionMaxLength: 500,
       nonprofitNameMaxLength: 50,
       errors: {},
+      warnings: {},
       selectedLocation: null,
       selectedPlace: null,
     }
@@ -126,6 +136,10 @@ export default {
 
     onFormValuesChanged () {
       this.$emit('detailsChanged', this.form)
+    },
+
+    onFieldWarning (field, warnings) {
+      this.warnings[field] = warnings
     },
 
     onPlaceChanged (place, location) {
@@ -159,7 +173,7 @@ export default {
   }
 
   label {
-    font-size: 20px;
+    font-size: 19px;
     font-weight: 700 !important;
     color: #4a4a4a;
     line-height: 1.4;
