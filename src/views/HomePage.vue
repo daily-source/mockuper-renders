5a21cdf9753d3dcca811e1b78773b4a597089a40
John Vee Uminga <johnveeuminga@example.com>
2019-06-22 13:36:39 +0800

    Homepage meta changes.

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -66,7 +66,7 @@ export default {
     const description = 'The modern way to help free slaves';
     const title = 'Virtual Railroad';
     return {
-      title: 'Home | Virtual Railroad',
+      title: 'Home - v8 | Virtual Railroad',
       meta: [
         { vmid: 'description', name: 'description', content: description },
         { vmid: 'og:title', property: 'og:title', content: title },
