578106a97b087c3654d5fbeb78073adabfe9b5ea
Camilo Delvasto <camilodelvasto@gmail.com>
2019-10-01 22:35:21 -0500

    Edit rollback yml, edit vue config

--- vue.config.js
+++ vue.config.js
@@ -23,4 +23,25 @@ module.exports = {
       },
     },
   },
+  pluginOptions: {
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: [
+        '/',
+        '/explore',
+        '/nonprofit',
+        'fundraiser'
+      ],
+      useRenderEvent: true,
+      headless: true,
+      onlyProduction: true,
+      postProcess: route => {
+        // Defer scripts and tell Vue it's been server rendered to trigger hydration
+        route.html = route.html
+          .replace(/<script (.*?)>/g, '<script $1 defer>')
+          .replace('id="app"', 'id="app" data-server-rendered="true"');
+        return route;
+      }
+    }
+  }
 };
