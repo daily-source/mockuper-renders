86ea76415068a2517b374dbd3b79424be4d59bb6
John Vee Uminga <johnveeuminga@example.com>
2019-10-09 13:21:59 +0800

    Fixed Nonprofit page.

--- src/views/Nonprofit.vue
+++ src/views/Nonprofit.vue
@@ -134,7 +134,7 @@ export default {
    */
   components: {
     SharedFooter: () => import('Components/Shared/SharedFooter.vue'),
-    AppHeader: () => import('Components/XthonFresh/AppHeader.vue'),
+    AppHeader: () => import('Components/VirtuousVacations/AppHeader.vue'),
     NonprofitHero: () => import('Components/nonprofit/NonprofitHero.vue'),
     DonateView: () => import('./DonateView.vue'),
     DonorsList: () => import('Components/general/DonorsList.vue'),
